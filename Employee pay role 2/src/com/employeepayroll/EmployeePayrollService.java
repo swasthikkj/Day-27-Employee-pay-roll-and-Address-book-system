02d44fa35cda11d717d7a9ed133822ea6ebc4f70
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-05-01 15:32:07 +0530

    program

---
+++ Employee pay role 2/src/com/employeepayroll/EmployeePayrollService.java
@@ -0,0 +1,49 @@
+package com.employeepayroll;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollService {
+
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	}
+
+	private List<EmployeePayroll> emplyeePayrollList;
+
+	public EmployeePayrollService() {
+
+	}
+
+	public EmployeePayrollService(List<EmployeePayroll> emplyeePayrollList) {
+		this.emplyeePayrollList = emplyeePayrollList;
+	}
+
+	private void showData() {
+		System.out.println("Employee data is : " +emplyeePayrollList);
+	}
+
+	private void writeData(Scanner data) throws IOException {
+
+		System.out.println("Enter your name : ");
+		String name = data.nextLine();
+		System.out.println("Enter your ID : ");
+		int id = data.nextInt();
+		System.out.println("Enter your salary : ");
+		double salary = data.nextDouble();
+
+		EmployeePayroll empData = new EmployeePayroll(id, name, salary);
+		emplyeePayrollList.add(empData);
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner input = new Scanner(System.in);
+		ArrayList<EmployeePayroll> emplyeePayrollList = new ArrayList<EmployeePayroll>();
+
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(emplyeePayrollList);
+		employeePayrollService.writeData(input);
+		employeePayrollService.showData();	
+	}
+}
